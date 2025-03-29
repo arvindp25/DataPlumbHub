401be5d9fb2dacafc19b1f9c7035000ea26ebe90
arvindp25 <officialarvinop@gmail.com>
2025-03-29 22:03:55 +0530

    source

--- pyspark/infra/main.tf
+++ pyspark/infra/main.tf
@@ -24,16 +24,22 @@ resource "google_storage_bucket" "pyspark_files" {
 #  uniform_bucket_level_access = true
 }
 
-resource "null_resource" "copy_file_code" {
-  triggers = { always_run = var.commit_hash }
-  provisioner "local-exec" {
-    command = <<-EOT
-      gcloud auth activate-service-account --key-file="gcp.json"
-      gsutil -m cp -r ../cymbal_investment_dataset/* gs://${google_storage_bucket.pyspark_files.name }/${var.commit_hash}/*
-    EOT
-  }
-
+resource "google_storage_bucket_object" "empty_folder" {
+  name   = "${var.commit_hash}/"
+  source =  "../cymbal_investment_dataset/*"         
+  bucket = google_storage_bucket.pyspark_files.name
 }
+
+# resource "null_resource" "copy_file_code" {
+#   triggers = { always_run = var.commit_hash }
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       gcloud auth activate-service-account --key-file="gcp.json"
+#       gsutil -m cp -r ../cymbal_investment_dataset/* gs://${google_storage_bucket.pyspark_files.name }/${var.commit_hash}/*
+#     EOT
+#   }
+
+# }
 
 # resource "google_service_account" "dataproc_service_account" {
 #   account_id   = "dataprocserverless"

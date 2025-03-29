ce7e18a7987bfbec47e5fa9949d7eb2ada751377
arvindp25 <officialarvinop@gmail.com>
2025-03-29 11:55:28 +0530

    initial code

--- pyspark/cymbal_investment_dataset/main.py
+++ pyspark/cymbal_investment_dataset/main.py
@@ -1,1 +1,21 @@
-print("gg")
+from pyspark.sql import SparkSession
+import os
+from py4j.protocol import Py4JJavaError
+
+spark = SparkSession.builder.master('yarn').appName("CymbalInvestmentPortfolio")
+
+bucket = os.getenv("STAGING_BUCKET")
+spark.conf.set("temporaryGcsBucket", bucket)
+
+source_table = os.getenv("BQ_SOURCE_TABLE")
+dest_table = os.getenv("BQ_DEST_TABLE")
+
+try:
+    table_df = spark.read.format('bigquery').option('table', source_table).option('inferSchema', "true")
+except Py4JJavaError as e:
+    print(f"Table {source_table} not found.")
+    raise
+
+agg_df = table_df.groupBy(["symbol", "tradedate"]).agg({"StrikePrice": "avg", "Quantity": "count"})
+
+agg_df.write.format('bigquery').option('table', dest_table)

fb6b00b249d775310ac3e81c54a176e52cd6f58a
arvindp25 <officialarvinop@gmail.com>
2025-03-30 12:26:47 +0530

    destroying infrastructurw

--- pyspark/cymbal_investment_dataset/main.py
+++ pyspark/cymbal_investment_dataset/main.py
@@ -3,29 +3,52 @@ import os
 from py4j.protocol import Py4JJavaError
 import sys
 
+staging_bucket = sys.argv[3]
+
 def clean_column_name(name):
     return name.replace("(", "_").replace(")", "").replace(" ", "_")
 
-source_table = sys.argv[1]
-dest_table = f"{sys.argv[2]}.cymbal-investment-portfolio"
-bucket = sys.argv[3]
-spark = SparkSession.builder.master('yarn').appName("CymbalInvestmentPortfolio").config("temporaryGcsBucket", bucket).getOrCreate()
-
-try:
-    table_df = spark.read.format('bigquery').option('table', source_table).option('inferSchema', "true").load()
-except Py4JJavaError as e:
-    print(f"Table {source_table} not found.")
-    raise
-print("Current df size:", (table_df.count(), len(table_df.columns)))
-print("Columns:", table_df.columns)
-
-agg_df = table_df.groupBy(["symbol", "tradedate"]).agg({"StrikePrice": "avg", "Quantity": "count"})
 
 
+def init_sparksession(appname = "CymbalInvestmentPortfolio" ):
+    return SparkSession.builder.master('yarn').appName("CymbalInvestmentPortfolio").config("temporaryGcsBucket", staging_bucket).getOrCreate()
 
-# Rename all columns dynamically
-new_column_names = {col: clean_column_name(col) for col in agg_df.columns}
-for old_name, new_name in new_column_names.items():
-    agg_df = agg_df.withColumnRenamed(old_name, new_name)
+def load_source_table (spark, source_table):
+    try:
+        table_df = spark.read.format('bigquery').option('table', source_table).option('inferSchema', "true").load()
+    except Py4JJavaError as e:
+        print(f"Table {source_table} not found.")
+        raise
+    print("Current df size:", (table_df.count(), len(table_df.columns)))
+    print("Columns:", table_df.columns)
 
-agg_df.write.format('bigquery').option('table', dest_table).mode("overwrite").save()
+    return table_df
+
+def agg_dataframe(df):
+    agg_df = df.groupBy(["symbol", "tradedate"]).agg({"StrikePrice": "avg", "Quantity": "count"})
+    return agg_df
+
+def dump_df_to_bq(df,dest_table):
+    df.write.format('bigquery').option('table', dest_table).mode("overwrite").save()
+
+def main():
+    source_table = sys.argv[1]
+    dest_table = f"{sys.argv[2]}.cymbal-investment-portfolio"
+
+
+    # init
+    spark = init_sparksession()
+    df = load_source_table(spark,source_table=source_table)
+    df = agg_dataframe(df)
+    # Rename all columns dynamically
+    new_column_names = {col: clean_column_name(col) for col in df.columns}
+    for old_name, new_name in new_column_names.items():
+        df = df.withColumnRenamed(old_name, new_name)
+
+    dump_df_to_bq(df, dest_table)
+
+
+        
+
+
+

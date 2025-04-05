49c0a4de61be6347a0f21e44bd321ba439c7f627
arvindp25 <officialarvinop@gmail.com>
2025-04-05 22:38:41 +0530

    gg

--- pyspark/infra/realttime.tf
+++ pyspark/infra/realttime.tf
@@ -125,17 +125,18 @@ resource "google_pubsub_subscription" "bigquery_subscription" {
     table = "${google_bigquery_table.iot_sensor_data.project}.${google_bigquery_table.iot_sensor_data.dataset_id}.${google_bigquery_table.iot_sensor_data.table_id}"
   }
 
-  depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
+  # depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
 }
 
-resource "google_project_iam_member" "editor" {
-  project = var.gcp_project_id
-  role   = "roles/bigquery.dataEditor"
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-}
+# these permission is provided with consloe
+# resource "google_project_iam_member" "editor" {
+#   project = var.gcp_project_id
+#   role   = "roles/bigquery.dataEditor"
+#   member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+# }
 
-resource "google_project_iam_member" "viewer" {
-  project = data.google_project.project.project_id
-  role   = "roles/bigquery.metadataViewer"
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-}
+# resource "google_project_iam_member" "viewer" {
+#   project = data.google_project.project.project_id
+#   role   = "roles/bigquery.metadataViewer"
+#   member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+# }

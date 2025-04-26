30519f667d86ec43f1affed965107d7bf40fa8f5
arvindp25 <officialarvinop@gmail.com>
2025-04-26 15:32:10 +0530

    gg

--- aws-terraform/infra/main.tf
+++ aws-terraform/infra/main.tf
@@ -21,10 +21,37 @@ resource "aws_s3_bucket" "test" {
   
 }
 
+data "aws_vpc" "default_vpc" {
+    default = true
+  
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssg"
+  description = "Allow ssh"
+  vpc_id = data.aws_vpc.default_vpc.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+}
+
 
 resource "aws_instance" "ec2_instance" {
     ami = "ami-0f1dcc636b69a6438"
     instance_type = "t2.micro"
-
+    security_groups = [aws_security_group.allow_ssh.id]
+    tags = {
+      Name = "ec2_instance"
+    }
   
 }

63dc0e9a3b039416c5bc44bb13e51cfb3f37c2a3
arvindp25 <officialarvinop@gmail.com>
2025-05-03 10:54:04 +0530

    gg

--- pyspark/infra/dataproc.tf
+++ pyspark/infra/dataproc.tf
@@ -92,7 +92,7 @@ resource "google_dataproc_job" "spark_streaming" {
   }
 
   pyspark_config {
-    main_python_file_uri = "gs://${google_storage_bucket.pyspark_files.name}/wikimdeia_streaming/${var.commit_hash}/spark-streaming.py"
+    main_python_file_uri = "gs://${google_storage_bucket.pyspark_files.name}/wikimedia_streaming/${var.commit_hash}/spark-streaming.py"
     args = ["--subscription_id, ${google_pubsub_subscription.wikimedia-subscription.name}",
             "--staging_bucket, ${google_storage_bucket.pyspark_staging_bucket.name}"
     ]

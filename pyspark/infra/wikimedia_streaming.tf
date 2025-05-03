e6879aeeb74f923e7594ef6002536bb9fd04741c
arvindp25 <officialarvinop@gmail.com>
2025-05-03 23:25:23 +0530

    gg

--- pyspark/infra/wikimedia_streaming.tf
+++ pyspark/infra/wikimedia_streaming.tf
@@ -78,7 +78,7 @@ resource "google_pubsub_subscription" "wikimedia-subscription" {
 
 
   # 20 minutes
-  message_retention_duration = "10m"
+  message_retention_duration = "600s"
   retain_acked_messages      = false
 
   ack_deadline_seconds = 20

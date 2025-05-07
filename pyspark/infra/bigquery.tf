87eb76ea48624e4622c1b3097605ad868dd57d84
arvindp25 <officialarvinop@gmail.com>
2025-05-07 22:18:53 +0530

    gg

--- pyspark/infra/bigquery.tf
+++ pyspark/infra/bigquery.tf
@@ -12,33 +12,33 @@ resource "google_bigquery_table" "iot_sensor_data" {
   schema = file("./iot_sensor_data_bq.json")
 }
 
-# resource "google_bigquery_table" "edit_per_count" {
-#   deletion_protection = false
-#   table_id   = "wikimedia_edit_per_count"
-#   dataset_id = google_bigquery_dataset.data_transformed.dataset_id
-#   schema =  <<EOF
-#   [{
-#     "name" : "window",
-#     "type" : "RECORD",
-#     "fields":[
-#     {
-#     "name":"start",
-#     "type": "TIMESTAMP"
-#     },
-#         {
-#     "name":"end",
-#     "type": "TIMESTAMP"
-#     }
-#     ]
-#     },
-#     {
-#       "name" : "edit_count",
-#       "type" : "INT64"
-#     }
-#   ]
-#   EOF
+resource "google_bigquery_table" "edit_per_count" {
+  deletion_protection = false
+  table_id   = "wikimedia_edit_per_count"
+  dataset_id = google_bigquery_dataset.data_transformed.dataset_id
+  schema =  <<EOF
+  [{
+    "name" : "window",
+    "type" : "RECORD",
+    "fields":[
+    {
+    "name":"start",
+    "type": "TIMESTAMP"
+    },
+        {
+    "name":"end",
+    "type": "TIMESTAMP"
+    }
+    ]
+    },
+    {
+      "name" : "edit_count",
+      "type" : "INT64"
+    }
+  ]
+  EOF
 
-# }
+}
 
 resource "google_bigquery_table" "rolling_avg" {
   deletion_protection = false
